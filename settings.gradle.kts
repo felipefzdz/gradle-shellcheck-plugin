82ee213a4e11054821180df1ca50a4ec941fccd7
Benedikt Ritter <benedikt@gradle.com>
2024-09-19 13:50:13 +0200

    Upgrade to latest develocity plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,14 +1,13 @@
 plugins {
-    id("com.gradle.enterprise") version "3.5"
+    id("com.gradle.develocity") version "3.18.1"
 }
 
 rootProject.name = "gradle-shellcheck-plugin"
 include("shellcheck")
 
-gradleEnterprise {
+develocity {
     buildScan {
-        publishAlways()
-        termsOfServiceUrl = "https://gradle.com/terms-of-service"
-        termsOfServiceAgree = "yes"
+        termsOfUseUrl = "https://gradle.com/terms-of-service"
+        termsOfUseAgree = "yes"
     }
 }
